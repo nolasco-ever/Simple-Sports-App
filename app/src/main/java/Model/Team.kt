ddad2664739b6bb3feeb2a5bd77f2ba9f866b0db
nolasco-ever <74639646+nolasco-ever@users.noreply.github.com>
2021-11-23 01:27:22 -0500

    Create Team Model

---
+++ app/src/main/java/Model/Team.kt
@@ -0,0 +1,10 @@
+package Model
+
+data class Team(
+    var teamName: String,
+    var teamSport: String,
+    var formedYear: String,
+    var teamLeague: String,
+    var teamDescription: String,
+    var teamCountry: String
+)
